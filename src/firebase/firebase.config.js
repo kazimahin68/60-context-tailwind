6711c701d606805c769858a7da0c9e381386ddba
kazimahin68 <kazimahin68@gmail.com>
2023-04-21 22:45:40 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdAO8rSXcw5OEFPrbpi8BM5_5pNrMs0Dk",
+  authDomain: "context-tailwind-673ee.firebaseapp.com",
+  projectId: "context-tailwind-673ee",
+  storageBucket: "context-tailwind-673ee.appspot.com",
+  messagingSenderId: "685887917677",
+  appId: "1:685887917677:web:97d51e58576af67a12f964"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
